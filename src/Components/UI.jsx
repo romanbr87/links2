1dcac9760f07fe5d1fa516cca3d105f04d5ffbcc
Roman Braverman <75772227+romanbr87@users.noreply.github.com>
2021-10-16 14:53:57 +0300

    Add files via upload

--- src/Components/UI.jsx
+++ src/Components/UI.jsx
@@ -3,7 +3,7 @@ import "../style.css"
 
 import React, { useState, useEffect } from 'react'
 import { Link } from 'react-router-dom';
-import DataTable from "./DataTable2";
+import DataTable from "./DataTable";
 
 export default function UI(props) {
   const [data, setData] = useState() 
@@ -11,7 +11,7 @@ export default function UI(props) {
   const [searchData, setSearchData] = useState()
   const [searchText, setsearchText] = useState("")
   const [check1, setCheck1] = useState (false)
-  const [location, setLocation] = useState ("")
+  const [check2, setCheck2] = useState (false)
 
   useEffect(() => {
     setData (props.data)
@@ -51,7 +51,9 @@ export default function UI(props) {
     if (txt.trim() === "") return;
 
     var flags = "dug" + ((!check1) ? "i" : ''); 
+    //if (check2) txt = "\W*((?i)" + txt + "(?-i))\W"
     var regEx = new RegExp (txt, flags)
+    //alert (aa);
 
     var arr = data.reduce((a1, c1) => {
       return [...a1, ...c1.links.reduce((a2, c2) => {
@@ -59,9 +61,7 @@ export default function UI(props) {
           
           /*return (!check1) ? e.site_name.toLowerCase().includes(txt.toLowerCase()) :
                             e.site_name.includes(txt)*/
-            
-          if (location === '') return e.site_name.match (regEx)
-          else return e.site_name.match (regEx) && e.location === location
+            return e.site_name.match (regEx)
         })
         
         if (subArr.length > 0) return [...a2, {cat: c2.cat, links: subArr}]
@@ -79,96 +79,108 @@ export default function UI(props) {
 
   if (data == null || data === "" || data === undefined) return <p>Null</p>
   return (
-    <div className="container text-center pull-center">
-      <div className="panel" id="panelForm">
-        
-        <div className="panel-heading">
-        <h3 id= "title" style = {{ fontWeight: "lighter", textDecoration: "underline" }}>קטלוג אתרים של מקומות תעסוקה וחברות כוח אדם</h3>
-        <br style = {{padding: "0", margin: "0 0 14% 0"}} />
-        <h4 className="text-right">{ data.reduce((acc,element) => acc + getLinksLength(element.links), 0) } רשומות  </h4>
+    <React.Fragment>
+    <nav className="navbar navbar-inverse navbar-fixed-top" style={{ textAlign: 'left' }}>
+      <div className="container-fluid pull-right">
+        <div className="nav-item">
+          <Link to="/1">אודות</Link>
+          <br/>
+          <Link to="/About">אודות</Link>
         </div>
-        
-        <div className="panel-body text-center pull-right 
-        col-lg-4 col-md-4 col-sm-12 col-xs-12">
-
-        <div className="input-group input-group-sm" id="searchForm" style={{ direction: "ltr" }} >
-          <span className="input-group-btn">
-            <button className="btn btn-default text-left" type="button" onClick={e => SearchInData(e)}>
-            <i className="glyphicon glyphicon-search"></i></button>
-          </span>
-          <input type="text" className="form-control" onKeyUp={ e => SearchInData(e) }
-          placeholder="חיפוש" id="searchText" name="searchText"/>
-        </div>
-        <hr style = {{padding: "0", margin: "0 0 2% 0"}} />
-        <div className="checkbox text-right" style={{marginTop: "2%"}}>
-            <label for="inlineCheckbox1" className="checkbox-inline">
-            <input type="checkbox" id="inlineCheckbox1" value="option1" 
-            onChange={e=> setCheck1(e.target.checked)} />גודל אות</label>
-        </div>      
-        <div className="radio text-right" style={{margin: "2% auto 3% auto"}}>
-            
-            <label className="radio-inline">
-            <input type="radio" name="inlineRadio" value=""
-            onClick={e=> setLocation("")} />כל הארץ</label>
-
-            <label className="radio-inline">
-            <input type="radio" name="inlineRadio" value="north" 
-            onClick={e=> setLocation("north")} />צפון</label>
-
-            <label className="radio-inline">
-            <input type="radio" name="inlineRadio" value="center" 
-            onClick={e=> setLocation("center")} />מרכז</label>
-
-            <label className="radio-inline">
-            <input type="radio" name="inlineRadio" value="south" 
-            onClick={e=> setLocation("south")} />דרום</label>
-        </div>
-
-        { (index !== -2) ? '':
-        <div className="list-group text-right" id="list1">
-        <Link to="#" className="list-group-item active">
-          <span className="badge text-left" style={{ float: 'left'}}>{ data.reduce((acc,element) => acc + getLinksLength(element.links), 0) }</span>
-          קטגוריות אתרים
-        </Link>
-
-        {
-          data.map ((e, i) => {
-            return (<Link className="list-group-item" to="#" key={i} a-key={i} onClick={ event => setTable(event) }>
-              <span className="badge" style={{ float: 'left'}} a-key={i} onClick={ event => setTable(event) }>{ getLinksLength(e.links) }</span>
-              {AdjustNum (i) + ". " + e.name}
-            </Link>) 
-          })
-        }
-        </div>
-        }
-
-        <select className="form-control" id="sel" onChange={ e => setIndex(e.target.value) } >
-            {
-              data.map ((e, i) => {
-                return <option key={i+1} value={i}>{AdjustNum(i) + ". " + e.name + " - " + getLinksLength(e.links) + " קישורים" }</option> 
-              })
-            }
-        </select>
-        </div>
-        </div>
-
-
-      <div className="col-lg-8 col-md-8 col-sm-12 col-xs-12 col-lg-offset-2 col-md-offset-2"
-      style={{ marginTop: "2%", marginBottom: "2%"}}>
-      <div className="panel panel-info" style={{border: "1px solid #bce8f1"}}>
-        <div className="panel-heading">
-        <h3 style = {{ fontWeight: "lighter", margin: '0', padding: '.5em 0 .5em 0'}}>
-        { (index !== -1) ? data[index].name: (searchData.links.length !== 0) ? "חיפוש: " + searchText: "לא נמצאו רשומות עבור: " + searchText } </h3>
-        </div>
-        <div className="panel-body"></div>
-        {
-          (index === -1) ?  
-          <DataTable AdjustNum={AdjustNum} soryByAtrr={soryByAtrr} data={searchData}></DataTable>:
-          <DataTable AdjustNum={AdjustNum} soryByAtrr={soryByAtrr} data={data[index]}></DataTable>              
-        }
       </div>
-      </div>         
+    </nav>
+          
+    <div className="container text-center" dir='rtl'>
+      <div className="jumbotron text-right" style={{ padding: '2em 0 0 0', borderRadius: '0'}}>
+        <h1 id="title" style={{ paddingBottom: ".1em", textAlign: 'center', textDecoration: 'underline', backgroundColor: "white"  }}>אינדקס מקומות תעסוקה</h1>
+        <p className="text-right">
+        { data.reduce((acc,element) => acc + getLinksLength(element.links), 0) } אתרים 
+        </p>
+
+        <div className="row"><div className="col-lg-8 col-lg-offset-2 col-xs-10 col-xs-offset-1">
+        <p className="text pull-center">
+        ברוכים הבאים לאתר הפיילוט של אינדקס מקומות התעסוקה וחברות כוח האדם הגדול ביותר בארץ. 
+כאן תוכלו למצאו ולהיחשף לרוב רוב מקומות התעסוקה המוכרים במשק הישראלי כמו גם חברות כוח אדם וגורמים נוספים בתחום התעסוקה. אתר נמצא בבנייה, בסופו תוכלו לקבל את המאגר בצורה מקוטלגת באופן מקצועי
+        </p></div></div>
+      </div>
+
+      <fieldset className="panel panel-info" style={{border: "1px solid #bce8f1"}} id="panelForm">
+      <div className="panel-heading"><h3 style = {{ fontWeight: 'bolder', margin: '0', padding: '0' }}>
+      123</h3></div>
+      <div className="panel-body table table-bordered table-striped table-hover table-condensed text-center">
+
+      <div className="input-group input-group-sm" id="searchForm" style={{ direction: "ltr" }} >
+        <span className="input-group-btn">
+          <button className="btn btn-default text-left" type="button" onClick={e => SearchInData(e)}>
+          <i className="glyphicon glyphicon-search"></i></button>
+        </span>
+        <input type="text" className="form-control" onKeyUp={ e => SearchInData(e) }
+        placeholder="חיפוש" id="searchText" name="searchText"/>
+      </div>
+    
+      <div className="checkbox text-right" style={{direction: "rtl"}}>
+          <input type="checkbox" id="inlineCheckbox1" value="option1" onChange={e=> setCheck1(e.target.checked)} />  
+          <label for="inlineCheckbox1">גודל אות</label>
+
+          <input type="checkbox" id="inlineCheckbox2" value="option2" onChange={e=> setCheck2(e.target.checked)} />  
+          <label for="inlineCheckbox2">חיפוש מדוייק</label>
+      </div>
+
+      { (index !== -2) ? '':
+      <div className="list-group text-right">
+      <Link to="#" className="list-group-item active">
+        <span className="badge text-left" style={{ float: 'left'}}>{ data.reduce((acc,element) => acc + getLinksLength(element.links), 0) }</span>
+        קטגוריות אתרים
+      </Link>
+
+      {
+        data.map ((e, i) => {
+          return (<Link className="list-group-item" to="#" key={i} a-key={i} onClick={ event => setTable(event) }>
+            <span className="badge" style={{ float: 'left'}} a-key={i} onClick={ event => setTable(event) }>{ getLinksLength(e.links) }</span>
+            {AdjustNum (i) + ". " + e.name}
+          </Link>) 
+        })
+      }
+      </div>
+      }
+
+      <select className="form-control" id="sel" onChange={ e => setIndex(e.target.value) } >
+          {
+            data.map ((e, i) => {
+              return <option key={i+1} value={i}>{AdjustNum(i) + ". " + e.name + " - " + getLinksLength(e.links) + " קישורים" }</option> 
+            })
+          }
+      </select>
+      </div>
+      </fieldset>
+
+      <fieldset className="panel panel-info" style={{border: "1px solid #bce8f1", marginTop: "1%"}}>
+        
+        <div className="panel-heading"><h3 style = {{ fontWeight: 'bolder', margin: '0', padding: '0.25em 0 0.25em 0'}}>
+        { (index !== -1) ? data[index].name: "חיפוש: " + searchText } </h3></div>
+        <table className="panel-body table table-bordered table-striped table-hover table-condensed text-center" id="testTable">
+          <thead>
+            <tr style={{backgroundColor: "yellow" }}>
+              <th className="numberCell">#</th>
+              <th className="nameCell">שם</th>
+              <th>קישור 2</th>
+              <th>דף פייסבוק</th>
+              <th>2דף פייסבוק</th>
+              <th>לינקדאין</th>
+            </tr>
+          </thead>
+            {
+              (index === -1) ?  
+              <DataTable AdjustNum={AdjustNum} soryByAtrr={soryByAtrr} data={searchData}></DataTable>:
+              <DataTable AdjustNum={AdjustNum} soryByAtrr={soryByAtrr} data={data[index]}></DataTable>              
+            }
+        </table>
+      </fieldset>
+
     </div>
-    )
+    
+    </React.Fragment>
+
+  )
 }
   
